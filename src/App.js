971297f4d03c8053407ce0c0f3d3d1510cd4528b
danielecortes89 <danielecortes.89@gmail.com>
2022-12-10 12:51:09 +0100

    Pequeña aplicación de objetivos. Falta solo CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+function GoalForm(props) {
+  const [formData, setFormData] = React.useState({goal:'', by:''})
+
+  const changeHandler = (e) => {
+    console.log(e.target);
+    setFormData({...formData, [e.target.name]:e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    console.log(e.target);
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({goal:'', by:''})
+    
+  }
+
+  return ( 
+    <>
+      <h1>My goals</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='goal' placeholder='Goal...' value={formData.goal} onChange={changeHandler}/>
+        <input type='text' name='by' placeholder='by...' value={formData.by} onChange={changeHandler} />
+        <button type="submit">Submit Goal</button>
+      </form>
+    </>
+
+  )
+
+}
+
+function ListOfGoals(props) {
+  
+  return(
+    <ul>
+      {props.allGoals.map((listItem) =>(           
+        <li key={listItem.goal}>
+          <span>My goal is to {listItem.goal} by {listItem.by}</span>
+          </li>
+      )
+      )}
+    </ul>
+  );
+}
+
+
 function App() {
+  const [allGoals, updateAllGoals] = React.useState([]);
+  console.log(allGoals)
+  
+
+
+  function addGoal(goal) {updateAllGoals([...allGoals, goal]);}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoalForm onAdd={addGoal} />
+      <ListOfGoals allGoals={allGoals}/>
+      
     </div>
   );
 }
